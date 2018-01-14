ddafd7ec410dad636a88c1bad24f342a8efb6dec
Austin <austin1911remo@gmail.com>
2018-01-14 16:02:02 -0500

    Set up Components and cleaned it up a bit
    Put in some components, nothing major yet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
 
+//Change logo later
 class App extends Component {
   render() {
     return (
+      <div>  
+        <Header />
+      </div>
+      );
+  }
+}
+
+//Header -> Nav, Body -> left_col + right_col
+class Header extends Component {
+  render() {
+    return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Aurora</h1>
+      <Nav />
       </div>
     );
   }
 }
+
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        Nav
+      </div>
+    );
+  }
+}
+
+class Body extends Component {
+  render() {
+    return (
+      <div>
+        Body
+      </div>
+    );
+  }
+}
+
+//ReactDOM.render(<Header />, document.getElementById('app'));
 
 export default App;
